c2b2f84977faadca28fc060021f4967d4261cacc
Lynette <linetnyambura45@gmail.com>
2025-09-14 19:45:08 +0300

    add task6 and deleted a file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,2 +1,7 @@
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+const x = Number(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) console.log("C is fun");
+}
